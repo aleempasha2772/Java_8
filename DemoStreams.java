50cb1a37227912c325e8389a7df287f402e66136
Aleem pasha <aleempasha1410@gmail.com>
2024-05-17 14:27:47 +0530

    Streams

---
+++ DemoStreams.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DemoStreams {
+    public static void main(String Args[]){
+        List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+        List<Integer> myList1 = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        List<Integer> myList2 = Arrays.asList(1,2,3,4,5,6);
+        List<Integer> myList3 = Arrays.asList(15,2,30,4,5,65);
+
+        myList
+                .stream()
+                .filter(s -> s.startsWith("c"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(i ->System.out.println(i));
+
+        myList1.stream()
+                .filter(i->i%2==0)
+                //.filter(i-> i%5==0)
+                .forEach(System.out::println);
+
+        System.out.println(myList2.stream()
+                .map(i-> i*2)
+                .reduce(0,(c,e)->c+e));
+
+        System.out.println(myList3.stream()
+                .filter(i->i%5 == 0)
+                .reduce(0,(c,e)->c+e)
+        );
+    }
+}
