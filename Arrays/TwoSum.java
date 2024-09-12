b292004a3ad81835db06fcf129af059c777caa3d
Aleem pasha <aleempasha1410@gmail.com>
2024-09-12 13:22:42 +0530

    HashMap Approch

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,5 +1,8 @@
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     /* Two-pointer approch */
@@ -19,10 +22,30 @@ public class TwoSum {
         }
         return null;
     }
+
+
+    /*
+    HashMap Approch
+     */
+    public static int[] twoSum2(int[] numbers, int target){
+        Map<Integer,Integer> mpp = new HashMap<>();
+        for(int i=0;i<numbers.length -1;i++) {
+            int complement = target - numbers[i];
+            if (mpp.containsKey(complement)) {
+                return new int[]{mpp.get(complement), i};
+            }
+            mpp.put(numbers[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
     public static void main(String args[]){
         int[] num = {2, 7, 11, 15};
         int tar = 9;
         int[] result = twoSum(num,tar);
-        System.out.print("["+result[0]+","+result[1]+"]");
+        int[] result1 = twoSum2(num,tar);
+        System.out.println("["+result[0]+","+result[1]+"]");
+        System.out.println("["+result1[0]+","+result1[1]+"]");
     }
 }
