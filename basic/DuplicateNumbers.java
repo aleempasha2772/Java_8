f6cde1f34faeaafb4fd90ea9d290685978e9051a
Aleem pasha <aleempasha1410@gmail.com>
2024-11-12 22:56:56 +0530

    hashset Approch

--- basic/DuplicateNumbers.java
+++ basic/DuplicateNumbers.java
@@ -1,8 +1,25 @@
 package basic;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class DuplicateNumbers {
+    public static List<Integer> duplicates(int[] nums){
+        Set<Integer> seen = new HashSet<>();
+        List<Integer> duplicates = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(seen.contains(nums[i])){
+                duplicates.add(nums[i]);
+            }else {
+                seen.add(nums[i]);
+            }
+        }
+        return duplicates;
+    }
+
+
     public static void main(String args[]){
         int[] numbers = {1, 2,3, 3, 4, 5,5,8,9,9};
         ArrayList<Integer> nums = new ArrayList<>();
@@ -15,5 +32,7 @@ public class DuplicateNumbers {
             }
         }
         System.out.println(nums);
+        System.out.println("Hashset Approch");
+        System.out.println(duplicates(numbers));
     }
 }
